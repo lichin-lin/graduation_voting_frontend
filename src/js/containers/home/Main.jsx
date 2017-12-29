81cf2b38cfab583e0b7fc101e80c030e77e31f8f
lichin-lin <vic20087cjimlin.cs03@nctu.edu.tw>
2017-12-30 02:44:25 +0800

    add mobile

--- src/js/containers/home/Main.jsx
+++ src/js/containers/home/Main.jsx
@@ -87,10 +87,10 @@ const SwiperWrapper = styled.div`
       transform: scale(1) translateY(0px);
     }
     .swiper-slide-prev {
-      transform: scale(0.5) translate(0vw, 50px) rotate(-15deg);
+      transform: scale(0.5) translate(0vw, -200px) rotate(-5deg);
     }
     .swiper-slide-next {
-      transform: scale(0.5) translate(0vw, 50px) rotate(15deg);
+      transform: scale(1.25) translate(15vw, 100px) rotate(5deg);
     }
   }
   .swiper-button-next,
@@ -143,8 +143,12 @@ const Cover = styled.div`
 `
 const CoverTitle = styled.h3`
   color: white;
-  font-weight: bold;
-  font-size: ${fontSize.p1};
+  font-weight: lighter;
+  letter-spacing: 1px;
+  font-size: ${fontSize.h3};
+  @media screen and (max-width: ${breakpoint.tablet}) {
+    font-size: ${fontSize.p1};
+  }
 `
 const ButtonGroup = styled.div`
   margin-top: 10px;
@@ -200,93 +204,93 @@ export default class Main extends Component {
     this.toggleChecked = this.toggleChecked.bind(this)
     this.swiper = null
   }
-  // componentDidMount () {
-  //   const canvas = document.getElementById('canvas')
-  //   const context = canvas.getContext('2d')
-  //   const colorPallete = ['#ff1783', '#17c9ff', '#36ff40']
-  //
-  //   var width = canvas.width = window.innerWidth
-  //   var height = canvas.height = window.innerHeight
-  //   var origin = {x: width / 2, y: height / 2}
-  //   var mouse = {x: width / 2, y: height / 2}
-  //   var balls = []
-  //   var count = 0
-  //   var randomCount = 1
-  //
-  //   window.onresize = () => {
-  //     width = canvas.width = window.innerWidth
-  //     height = canvas.height = window.innerHeight
-  //     origin = {x: width / 2, y: height / 2}
-  //   }
-  //
-  //   class Ball {
-  //     constructor () {
-  //       this.x = origin.x
-  //       this.y = origin.y
-  //       this.angle = Math.PI * 2 * Math.random()
-  //       this.vx = (1.3 + Math.random() * 0.3) * Math.cos(this.angle)
-  //       this.vy = (1.3 + Math.random() * 0.3) * Math.sin(this.angle)
-  //       this.r = 6 + 3 * Math.random()
-  //       this.color = colorPallete[Math.floor(Math.random() * colorPallete.length)]
-  //     }
-  //
-  //     update () {
-  //       this.x += this.vx
-  //       this.y += this.vy
-  //       this.r -= 0.01
-  //     }
-  //   }
-  //
-  //   loop()
-  //   function loop () {
-  //     context.clearRect(0, 0, width, height)
-  //     if (count === randomCount) {
-  //       balls.push(new Ball())
-  //       count = 0
-  //       randomCount = 5 + Math.floor(Math.random() * 5)
-  //     }
-  //     count++
-  //     for (var i = 0; i < balls.length; i++) {
-  //       var b = balls[i]
-  //       context.fillStyle = b.color
-  //       context.beginPath()
-  //       context.arc(b.x, b.y, b.r, 0, Math.PI * 2, false)
-  //       context.fill()
-  //       b.update()
-  //     }
-  //
-  //     origin.x += (mouse.x - origin.x) * 0.15
-  //     origin.y += (mouse.y - origin.y) * 0.15
-  //
-  //     context.fillStyle = '#ffdd02'
-  //     context.beginPath()
-  //     context.arc(origin.x, origin.y, 20, 0, Math.PI * 2, false)
-  //     context.fill()
-  //
-  //     removeBall()
-  //     requestAnimationFrame(loop)
-  //   }
-  //
-  //   function removeBall () {
-  //     for (var i = 0; i < balls.length; i++) {
-  //       var b = balls[i]
-  //       if (
-  //         b.x + b.r < 0 ||
-  //         b.x - b.r > width ||
-  //         b.y + b.r < 0 ||
-  //         b.y - b.r > height ||
-  //         b.r <= 0
-  //       ) {
-  //         balls.splice(i, 1)
-  //       }
-  //     }
-  //   }
-  //
-  //   window.addEventListener('mousemove', (e) => {
-  //     mouse.x = e.clientX
-  //     mouse.y = e.clientY
-  //   }, false)
-  // }
+  componentDidMount () {
+    const canvas = document.getElementById('canvas')
+    const context = canvas.getContext('2d')
+    const colorPallete = ['#ff1783', '#17c9ff', '#36ff40']
+
+    var width = canvas.width = window.innerWidth
+    var height = canvas.height = window.innerHeight
+    var origin = {x: width / 2, y: height / 2}
+    var mouse = {x: width / 2, y: height / 2}
+    var balls = []
+    var count = 0
+    var randomCount = 1
+
+    window.onresize = () => {
+      width = canvas.width = window.innerWidth
+      height = canvas.height = window.innerHeight
+      origin = {x: width / 2, y: height / 2}
+    }
+
+    class Ball {
+      constructor () {
+        this.x = origin.x
+        this.y = origin.y
+        this.angle = Math.PI * 2 * Math.random()
+        this.vx = (1.3 + Math.random() * 0.3) * Math.cos(this.angle)
+        this.vy = (1.3 + Math.random() * 0.3) * Math.sin(this.angle)
+        this.r = 6 + 3 * Math.random()
+        this.color = colorPallete[Math.floor(Math.random() * colorPallete.length)]
+      }
+
+      update () {
+        this.x += this.vx
+        this.y += this.vy
+        this.r -= 0.01
+      }
+    }
+
+    loop()
+    function loop () {
+      context.clearRect(0, 0, width, height)
+      if (count === randomCount) {
+        balls.push(new Ball())
+        count = 0
+        randomCount = 5 + Math.floor(Math.random() * 5)
+      }
+      count++
+      for (var i = 0; i < balls.length; i++) {
+        var b = balls[i]
+        context.fillStyle = b.color
+        context.beginPath()
+        context.arc(b.x, b.y, b.r, 0, Math.PI * 2, false)
+        context.fill()
+        b.update()
+      }
+
+      origin.x += (mouse.x - origin.x) * 0.15
+      origin.y += (mouse.y - origin.y) * 0.15
+
+      context.fillStyle = '#ffdd02'
+      context.beginPath()
+      context.arc(origin.x, origin.y, 20, 0, Math.PI * 2, false)
+      context.fill()
+
+      removeBall()
+      requestAnimationFrame(loop)
+    }
+
+    function removeBall () {
+      for (var i = 0; i < balls.length; i++) {
+        var b = balls[i]
+        if (
+          b.x + b.r < 0 ||
+          b.x - b.r > width ||
+          b.y + b.r < 0 ||
+          b.y - b.r > height ||
+          b.r <= 0
+        ) {
+          balls.splice(i, 1)
+        }
+      }
+    }
+
+    window.addEventListener('mousemove', (e) => {
+      mouse.x = e.clientX
+      mouse.y = e.clientY
+    }, false)
+  }
 
   goNext = () => {
     console.log('next')
@@ -343,7 +347,7 @@ export default class Main extends Component {
               <Cover>
                 <img src={require('assets/image/a4.jpg')} />
               </Cover>
-              <CoverTitle>Crowd Lu -<br/> Fish</CoverTitle>
+              <CoverTitle>Crowd Lu - Fish</CoverTitle>
             </AlbumWrapper>
 
             <AlbumWrapper>
