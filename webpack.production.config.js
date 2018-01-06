345444dd28570814909b1c1226bbd957d2aba996
lichin-lin <vic20087cjimlin.cs03@nctu.edu.tw>
2018-01-06 19:19:53 +0800

    config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -14,7 +14,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[chunkhash].js',
     chunkFilename: '[id].[chunkhash].js',
-    publicPath: '/'
+    publicPath: 'graduation_voting_frontend/'
   },
   devtool: false,
   resolve: {
